d6546950ee13d371ebe093b0c059b1e0a19d7dc1
vxdcxw <defresne.thierry@gmail.com>
2022-11-07 08:19:08 +0100

    Remove origin of the map from the grid
    It was used only for debugging purposes

--- src/generator/map.rs
+++ src/generator/map.rs
@@ -12,7 +12,6 @@ enum Tile {
     Empty,
     Floor,
     Door,
-    Origin,
 }
 
 impl Map {
@@ -34,7 +33,6 @@ impl Map {
             match i {
                 Tile::Floor => "x",
                 Tile::Door => "o",
-                Tile::Origin => "@",
                 _ => ".",
             }
         }).collect();
@@ -83,7 +81,6 @@ impl Map {
                 self.grid[(x + y * self.width as u32) as usize] = Tile::Floor;
             }
         }
-        self.grid[(self.offset.x + self.offset.y * self.width as i8) as usize] = Tile::Origin;
     }
 
     pub fn add_door(&mut self, position: &Vector<i8>){
