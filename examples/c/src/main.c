25088011f2fbc32d62244b738f4cdcff76041bc8
vxdcxw <defresne.thierry@gmail.com>
2022-11-19 00:59:04 +0100

    Rewrite C example for readability

--- examples/c/src/main.c
+++ examples/c/src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct Handle Handle;
 
@@ -30,44 +31,70 @@ extern char* map_as_string(Handle *handle);
 extern char* map_as_bytes(Handle *handle);
 extern Config* get_config();
 
-int main()
+void print_ascii(char* map)
 {
-	Config* config = get_config();
+	printf("%s\n", map);
+	//printf("Generated map (ASCII): %s\n", map);
+}
 
-	config->seed = 42;
-	config->rooms_count = 11;
-	config->rooms_min_size = (Vector) { 4, 4 };
-	config->rooms_max_size = (Vector) { 7, 7 };
-	config->rooms_spacing = (MinMax) { 3, 5 };
-	config->path_extension = (MinMax) { 2, 4 };
-
-	// TODO - Add config check
-
-	printf("Map seed: %u\n", config->seed);
-	printf("Number of rooms: %zu\n", config->rooms_count);
-
-    Handle* handle = map_create(config);
-	Vector size = map_size(handle);
-	char* map_ascii = map_as_string(handle);
-	char* map_bytes = map_as_bytes(handle);
-
-	printf("Map size: %ux%u\n", size.x, size.y);
-	printf("\n");
-	printf("Generated map (ASCII): %s\n", map_ascii);
-	printf("\n");
+void print_bytes(char* map, Vector size)
+{
 	printf("Generated map (bytes):\n");
 
 	for (int y = 0; y < size.y; y++)
 	{
 		for (int x = 0; x < size.x; x++)
 		{
-			printf("%i", map_bytes[x + y * size.x]);
+			printf("%i", map[x + y * size.x]);
 		}
 
 		printf("\n");
 	}
+}
+
+Config* create_config(int seed)
+{
+	Config* config = get_config();
+
+	config->seed = seed;
+	config->rooms_count = 7;
+	config->rooms_min_size = (Vector) { 4, 4 };
+	config->rooms_max_size = (Vector) { 7, 7 };
+	config->rooms_spacing = (MinMax) { 3, 5 };
+	config->path_extension = (MinMax) { 2, 4 };
+
+	return config;
+}
+
+void print_map(Handle* handle, bool draw_ascii, bool draw_bytes)
+{
+	if (draw_ascii)
+	{
+		print_ascii(map_as_string(handle));
+	}
+
+	if (draw_bytes)
+	{
+		print_bytes(map_as_bytes(handle), map_size(handle));
+	}
+
+	printf("\n");
+}
+
+void draw_map(uint32_t seed)
+{
+	Config* config = create_config(seed);
+	Handle* handle = map_create(config);
+
+	printf("Map %u", seed);
+	print_map(handle, true, false);
 
 	map_destroy(handle);
+}
+
+int main()
+{
+	draw_map(42);
 
 	return 0;
 }
